e1d9574cc84a0268d9c1645597ba33bdbd24075a
amaliujia <amaliujia@163.com>
2014-12-20 17:13:27 -0500

    [SD Communication] master heartbeats

--- src/Heartbeats/SDMaterHeartbeats.java
+++ src/Heartbeats/SDMaterHeartbeats.java
@@ -3,9 +3,14 @@ package Heartbeats;
 import Master.SDSlave;
 import Util.SDUtil;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
+import java.net.SocketTimeoutException;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 
 /**
  * Created by amaliujia on 14-12-20.
@@ -14,20 +19,57 @@ public class SDMaterHeartbeats implements Runnable{
 
     private ArrayList<SDSlave> slaveList;
     private DatagramSocket listener;
+    private HashMap<Integer, Boolean> responderList;
 
     public SDMaterHeartbeats(ArrayList<SDSlave> list){
         slaveList = list;
+        responderList = new HashMap<Integer, Boolean>();
     }
 
     public void run() {
 
     }
 
-    private void startListening(){
+    private void startListening(long currentTime) throws IOException {
         try {
             listener = new DatagramSocket(SDUtil.heatbeatsPort);
         } catch (SocketException e) {
             e.printStackTrace();
         }
+
+        //if UDP socket creates successfully
+        responderList.clear();
+        if(listener != null){
+
+            //prepare buffer
+            byte[] buf = new byte[1024];
+            DatagramPacket packet = new DatagramPacket(buf, 1024);
+
+            while(true){
+                //next timeout computation
+                long interval = getCurrentTimeInMillionSeconds() - currentTime;
+                int timeout = (int) (SDUtil.heartbeatsIntervalMillionSeconds - interval);
+                if(timeout <= 0){
+                    break;
+                }
+
+                //start listening
+                try {
+                    listener.setSoTimeout(timeout);
+                    listener.receive(packet);
+                    //TODO: what should I do now?
+                    String receive = new String(packet.getData(), 0, packet.getLength());
+                    String[] args = receive.split(" ");
+                } catch (SocketTimeoutException e){
+                    System.err.println("UDP socket time out");
+                }
+            }
+            listener.close();
+        }
+    }
+
+    private long getCurrentTimeInMillionSeconds(){
+         Date currentData = new Date();
+         return currentData.getTime();
     }
 }
--- src/Util/SDUtil.java
+++ src/Util/SDUtil.java
@@ -4,6 +4,7 @@ package Util;
  * Created by amaliujia on 14-12-20.
  */
 public class SDUtil {
-   public static final int masterLinstenerPort = 16640;
-   public static final int heatbeatsPort = 16641;
+    public static final int masterLinstenerPort = 16640;
+    public static final int heatbeatsPort = 16641;
+    public static final long heartbeatsIntervalMillionSeconds = 10000000;
 }
