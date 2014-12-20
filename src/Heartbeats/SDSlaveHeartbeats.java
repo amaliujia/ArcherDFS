4acd14ad9cedc3e012e54f5ae62deb4bdf2d07c4
Kang Huang <kanghuangnj@gmail.com>
2014-12-20 16:45:24 -0500

    [HK init]

---
+++ src/Heartbeats/SDSlaveHeartbeats.java
@@ -0,0 +1,8 @@
+package Heartbeats;
+
+/**
+ * Created by kanghuang on 12/20/14.
+ */
+interface SDSlaveHeartBeat {
+    public abstract void heartBeatsResponse();
+}
---
+++ src/Slave/SDSlaveNode.java
@@ -0,0 +1,10 @@
+package Slave;
+
+
+import Heartbeats.*;
+
+/**
+ * Created by kanghuang on 12/20/14.
+ */
+public class SDSlaveNode implements  {
+}
