88f83cbcbcd89f7dbe0526a9b03602a287fca5d2
amaliujia <amaliujia@163.com>
2014-12-26 20:05:10 -0500

    [SD System] add new entrances in main

--- src/SDArcherMain.java
+++ src/SDArcherMain.java
@@ -7,18 +7,23 @@ import Util.SDUtil;
  */
 public class SDArcherMain {
     public static void main(String[] args) throws Exception {
-        if(args.length == 0){
+        if(args.length == 0) {
+            SDUtil.fatalError("Wrong arguments for main function");
+        }else if(args[0].equals("master")){
             //start master
             SDMasterNode master = new SDMasterNode();
             master.startService(SDUtil.masterLinstenerPort);
-        }else if(args.length == 2){
+        }else if(args[0].equals("slave")){
             //start salve
-            HKSlaveNode slave = new HKSlaveNode(args[0], Integer.parseInt(args[1]));
+            HKSlaveNode slave = new HKSlaveNode(args[1], Integer.parseInt(args[2]));
             slave.connect();
             new Thread(slave).start();
+        }else if(args[0].equals("RMIServer")){
+            //TODO: start rmiserver
+        }else if(args[0].equals("Registry")){
+            //TODO: start registry
         }else{
             System.out.println("Arguments wrong");
         }
-        //TODO: add entrance to Registry
     }
 }
