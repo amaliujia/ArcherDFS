abbc4807bde414cc27fed6fd578c3dfc21bb4bc2
Kang Huang <kanghuangnj@gmail.com>
2015-01-04 20:22:17 -0500

    HK correct something

---
+++ src/FileSystem/Slave/SDSlaveHeartbeatsJob.java
@@ -0,0 +1,39 @@
+package FileSystem.Slave;
+
+import FileSystem.Util.SDDFSConstants;
+import Protocol.MasterService.SDMasterService;
+
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.Registry;
+
+/**
+ * Created by amaliujia on 15-1-2.
+ */
+public class SDSlaveHeartbeatsJob implements Runnable{
+
+    private SDSlaveIO slaveIO;
+
+    private Registry registry;
+
+    public SDSlaveHeartbeatsJob(SDSlaveIO IO, Registry registry){
+        this.registry = registry;
+        this.slaveIO = IO;
+    }
+
+
+    @Override
+    public void run() {
+        System.out.println("Heart beat");
+        try {
+            SDMasterService masterService = (SDMasterService)
+                    registry.lookup(SDMasterService.class.getCanonicalName());
+            masterService.heartbeat("slave", "localhost", SDDFSConstants.DEFAULT__SLAVE_REGISTRY_PORT,
+                                    slaveIO.getChunkNumber(), true);
+        } catch (RemoteException e) {
+            System.err.println("master node error");
+        } catch (NotBoundException e) {
+            System.err.println("master service not found");
+        }
+    }
+}
